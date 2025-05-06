aa8a599f521d1bdbecb98a7ad87e4aae1e5b6d4b
Nishita-codes <nishitapatidar03@gmail.com>
2025-05-07 01:02:07 +0530

    added files

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
 
     return (
        
-            <nav className={`fixed top-0 left-0 w-full bg-[#201f1f]/70 flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${isScrolled ? "bg-white/80 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-4" : "py-4 md:py-6"}`}>
+            <nav className={`fixed top-0 left-0 w-full bg-[#201f1f]/40 flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${isScrolled ? "bg-white/80 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-4" : "py-4 md:py-6"}`}>
 
                 {/* Logo */}
                 <Link to='/' >
