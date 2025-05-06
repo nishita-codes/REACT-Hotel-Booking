e3db25899a04e6b808f379e6fdfd68f7cf8d17e2
Nishita-codes <nishitapatidar03@gmail.com>
2025-05-07 00:34:28 +0530

    added files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero'
+import FeaturedDestination from '../Components/FeaturedDestination'
+import ExclusiveOffers from '../Components/ExclusiveOffers'
 
 const Home = () => {
   return (
     <>
       <Hero/>
+      <FeaturedDestination/>
+      <ExclusiveOffers/>
     </>
   )
 }
