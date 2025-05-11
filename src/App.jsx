456d1a649fd3104b4d54b763b6c2a94d47c97949
Nishita-codes <nishitapatidar03@gmail.com>
2025-05-11 17:19:40 +0530

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home';
 import Footer from './Components/Footer';
 import AllRoom from './pages/AllRoom';
+import RoomDetails from './pages/RoomDetails';
 
 const App = () => {
 
@@ -15,6 +16,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/rooms' element={<AllRoom/>} />
+          <Route path='/rooms/:id' element={<RoomDetails/>} />
         </Routes>
       </div>
       <Footer/>
