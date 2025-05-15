8867e93acc40aab4d26dc2303c7d66ce0259e045
Nishita-codes <nishitapatidar03@gmail.com>
2025-05-16 00:14:25 +0530

    added files

--- src/pages/RoomDetails.jsx
+++ src/pages/RoomDetails.jsx
@@ -1,27 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { assets, roomsDummyData } from '../assets/assets';
+import { assets, facilityIcons, roomsDummyData } from '../assets/assets';
 import StarRating from '../Components/StarRating';
 
 const RoomDetails = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     const [room, setRoom] = useState(null);
     const [mainImage, setMainImage] = useState(null);
-     
-    console.log("hey" , roomsDummyData)
-    
-    useEffect(() => {
-    const foundRoom = roomsDummyData.find(room => room._id.toString() === id);
-    console.log("foundroom →", foundRoom);
 
-    if (foundRoom) {
-        setRoom(foundRoom);
-        setMainImage(foundRoom.images[0]);
-    } else {
-        console.warn("No room found for id:", id);
-    }
-}, []);
-   
+    console.log("hey", roomsDummyData)
+
+    useEffect(() => {
+        const foundRoom = roomsDummyData.find(room => room._id.toString() === id);
+        console.log("foundroom →", foundRoom);
+
+        if (foundRoom) {
+            setRoom(foundRoom);
+            setMainImage(foundRoom.images[0]);
+        } else {
+            console.warn("No room found for id:", id);
+        }
+    }, []);
+
 
 
     return room && (
@@ -34,29 +34,73 @@ const RoomDetails = () => {
             </div>
 
             {/* room rating */}
-             <div className='flex item-center gap-1 mt-2'>
-                <StarRating/>
+            <div className='flex item-center gap-1 mt-2'>
+                <StarRating />
                 <p className='ml-2'>200+ reviews</p>
-             </div>
+            </div>
 
-             {/* room address */}
-             <div className='flex items-center gap-1 text-gray-500 mt-2'>
+            {/* room address */}
+            <div className='flex items-center gap-1 text-gray-500 mt-2'>
                 <img src={assets.locationIcon} alt="location-Icon" />
                 <span>{room.hotel.address}</span>
-             </div>
+            </div>
 
-             {/* room images */}
-             <div className='flex flex-col lg:flex-row mt-6 gap-6'>
+            {/* room images */}
+            <div className='flex flex-col lg:flex-row mt-6 gap-6'>
                 <div className='lg:w-1/2 w-full'>
                     <img src={mainImage} alt="Room Image" className='w-full rounded-xl shadow-lg object-cover' />
                 </div>
                 <div className='grid grid-cols-2 gap-4 lg:w-1/2 w-full'>
-                    {room?.images.length > 1 && room.images.map((image,index)=>(
-                        <img  onClick={()=>setMainImage(image)}  key={index} src={image} alt='Room Image' 
-                        className={`w-full rounded-xl shadow-md object-cover cursor-pointer ${mainImage === image && 'outline-3 outline-orange-500'}`}/>
+                    {room?.images.length > 1 && room.images.map((image, index) => (
+                        <img onClick={() => setMainImage(image)} key={index} src={image} alt='Room Image'
+                            className={`w-full rounded-xl shadow-md object-cover cursor-pointer ${mainImage === image && 'outline-3 outline-orange-500'}`} />
                     ))}
                 </div>
-             </div>
+            </div>
+
+            {/* room highlights */}
+            <div className='flex flex-col md:flex-row md:justify-between mt-10'>
+                <div className='flex flex-col'>
+                    <h1 className='text-3xl md:text-4xl font-playfair'>Experience Luxury Like Never Before</h1>
+                    <div className='flex flex-wrap items-center mt-3 mb-6 gap-4'>
+                        {room.amenities.map((item, index) => (
+                            <div key={index} className='flex items-center gap-2 px-3 py-2 rounded-lg bg-gray-100'>
+                                <img src={facilityIcons[item]} alt={item} className='w-5 h-5' />
+                                <p className='text-xs'>{item}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                {/* room price */}
+                <p className='text-2xl font-medium'>${room.pricePerNight}/night</p>
+            </div>
+
+            {/* checkin checkout form */}
+            <form className='flex flex-col md:flex-row items-start md:items-center justify-between bg-white shadow-[0px_0px_20px_rgba(0,0,0,0.15)] p-6 rounded-xl mx-auto mt-16 max-w-6xl'>
+              <div className='flex flex-col flex-wrap md:flex-row items-start md:items-center gap-4 md:gap-10 text-gray-500'>
+
+                 <div className='flex flex-col'>
+                    <label htmlFor="checkInDate" className='font-medium'>Check-In</label>
+                    <input type="date" name="" id="checkInDate" placeholder='Check-In' className='w-full rounded border border-gray-500 px-3 py-2 mt-1.5 outline-none' required/>
+                 </div>
+
+                 <div className='flex flex-col'>
+                    <label htmlFor="checkOutDate" className='font-medium'>Check-Out</label>
+                    <input type="date" name="" id="checkOutDate" placeholder='Check-Out' className='w-full rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required/>
+                 </div>
+
+                 <div className='flex flex-col'>
+                    <label htmlFor="guests" className='font-medium'>Guests</label>
+                    <input type="date" name="" id="guests" placeholder='0' className='max-w-20 rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none' required/>
+                 </div>
+
+              </div>
+              <button type='submit' className='bg-primary hover:bg-primary-dull active:scale-95 transition-all text-white rounded-md max-md:w-full max:md:mt-6 md:px-25 py-3 md:py-4 text-base cursor-pointer'>
+                Book Now
+              </button>
+            </form>
+
+
         </div>
 
     )
