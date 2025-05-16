f8f3c0ad02155b28a2d546fa679323eaf51ffdd2
Nishita-codes <nishitapatidar03@gmail.com>
2025-05-16 14:11:36 +0530

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import Footer from './Components/Footer';
 import AllRoom from './pages/AllRoom';
 import RoomDetails from './pages/RoomDetails';
+import MyBookings from './pages/MyBookings';
 
 const App = () => {
 
@@ -17,6 +18,8 @@ const App = () => {
           <Route path='/' element={<Home/>} />
           <Route path='/rooms' element={<AllRoom/>} />
           <Route path='/rooms/:id' element={<RoomDetails/>} />
+          <Route path='/my-bookings' element={<MyBookings/>} />
+
         </Routes>
       </div>
       <Footer/>
